d567466f4a13e484368efbd3dd196774a3c9ace7
fannybello <85925260+fannybello@users.noreply.github.com>
2021-06-18 02:18:37 -0500

    Add files via upload

---
+++ EjemploHolaMundo.java
@@ -0,0 +1,12 @@
+
+package ejemploholamundo;
+
+public class EjemploHolaMundo {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+        System.out.println("Diana Estefanía Bello Abarca");
+    }
+    
+}
